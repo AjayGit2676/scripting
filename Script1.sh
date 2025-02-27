e22c402a6dc16319168ef777e80621c19d13a7a8
AjayGit2676 <ajaytikar01@gmail.com>
2025-02-27 22:18:35 +0530

    change by Sham

--- Script1.sh
+++ Script1.sh
@@ -2,9 +2,8 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
 echo "Hello, Good morning!"
-
-
-
+echo "we are Larning Git"
+echo "we are Larning Devops"
 
 
 
