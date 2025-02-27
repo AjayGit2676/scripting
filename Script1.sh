e4aec57b8dc94710a9edcd05cd04e88247a4352c
AjayGit2676 <ajaytikar01@gmail.com>
2025-02-27 21:49:45 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,8 +2,8 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
 echo "Hello, Good morning!"
-echo "We are learning Github and Git"
-eho "This change done by me"
+
+
 
 
 
